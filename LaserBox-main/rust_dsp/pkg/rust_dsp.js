1753c3d0e20abf32b519ba7a3a4048d4c4f3b38b
HttpsL0ser <kaidynjscottknapik@gmail.com>
2026-09-05 23:49:46 -0400

    Delete LaserBox-main directory

--- LaserBox-main/rust_dsp/pkg/rust_dsp.js
+++
@@ -1,9 +0,0 @@
-/* @ts-self-types="./rust_dsp.d.ts" */
-import * as wasm from "./rust_dsp_bg.wasm";
-import { __wbg_set_wasm } from "./rust_dsp_bg.js";
-
-__wbg_set_wasm(wasm);
-wasm.__wbindgen_start();
-export {
-    ColourizerInstance, ColourizerInstanceParams, CompressorInstance, CompressorInstanceParams, CompressorParams, DspBuffer, FlangerInstance, FlangerInstanceParams, PhaserAlgorithmMode, PhaserInstance, PhaserInstanceParams, start
-} from "./rust_dsp_bg.js";
